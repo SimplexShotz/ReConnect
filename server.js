a70c05ffaeb588da9385c101bedab26efd62805e
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2020-02-01 23:36:55 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = 'ec2-3-132-213-41.us-east-2.compute.amazonaws.com';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
