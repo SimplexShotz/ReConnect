9486399ff1887df486a610c1bdcd8b7822cec1b4
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2020-02-02 00:16:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/javascript');
-  res.end('console.log("oof");');
+  console.log(req);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
